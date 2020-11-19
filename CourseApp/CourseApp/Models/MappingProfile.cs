6c924cb4fc3ebbd69b4cbd39cbe2edaa80fdca2c
Lamont Dutcher <dutcherl@csp.edu>
2020-11-19 15:11:23 -0600

    Fixed registration error where the mapping was not two-way

--- CourseApp/CourseApp/Models/MappingProfile.cs
+++ CourseApp/CourseApp/Models/MappingProfile.cs
@@ -9,7 +9,7 @@ namespace CourseApp.Models
         public MappingProfile()
         {
             CreateMap<UserModel, RegistrationVM>()
-                .ForMember(u => u.Email, opt => opt.MapFrom(x => x.UserName));
+                .ForMember(u => u.Email, opt => opt.MapFrom(x => x.UserName)).ReverseMap();
             CreateMap<UserModel, UserVM>()
                 .ForMember(d => d.Courses, o => o.MapFrom(s => s.UserCourses.Select(x => x.User)));
             CreateMap<CourseModel, CourseVM>()
